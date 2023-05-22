f751ffae3695fbdefeb89eadf745b631691a457d
Danny <123@email.com>
2023-05-21 21:38:21 -0400

    Frontend : page erreur 404.

--- public/taches-app/src/views/Erreur404.vue
+++ public/taches-app/src/views/Erreur404.vue
@@ -1,3 +1,16 @@
 <template>
-  <h1>Erreur 404</h1>
+  <main>
+    <h1>Erreur 404</h1>
+    <p>Cette page n'existe pas.</p>
+  </main>
 </template>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: calc(50vh - var(--header-height));
+}
+</style>
