d8fbe8eeafb69c31a843f571cecbb798525a7ed3
Danny <123@email.com>
2023-05-23 16:23:09 -0400

    Frontend : modification de la barre de navigation.

--- public/taches-app/src/App.vue
+++ public/taches-app/src/App.vue
@@ -1,14 +1,29 @@
 <script setup lang="ts">
+import { computed } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
 import './assets/css/global.css'
+
+import { useTableauStore } from './stores/store'
+
+/**
+ * Vérifie si l'utilisateur est connecté
+ * @returns True si l'utilisateur est connecté, false sinon
+ */
+const isLoggedIn = computed(() => {
+  return useTableauStore().getJwt() !== null
+})
 </script>
 
 <template>
   <header>
-    <nav>
+    <nav v-if="!isLoggedIn">
       <RouterLink to="/tableaux" class="logo">Tableaux</RouterLink>
       <RouterLink to="/inscription">Inscription</RouterLink>
       <RouterLink to="/connexion">Connexion</RouterLink>
+    </nav>
+    <nav v-else="isLoggedIn">
+      <RouterLink to="/tableaux" class="logo">Tableaux</RouterLink>
+      <a>Connecté</a>
     </nav>
   </header>
 
